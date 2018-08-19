dcbe606ce0bf2a3b6e1e801663758c7ac44111dc
Marco Sanchez <satchito@gmail.com>
2018-08-19 02:20:18 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -29,3 +29,11 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
   return array;
 }
 
+function removeElementFromBeginningOfArray(array) {
+  array.slice(1);
+  return array;
+}
+
+
+
+
