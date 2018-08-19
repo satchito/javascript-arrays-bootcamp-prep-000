972dce33ea4f38e3dabea75e9d5c4dde1f74412b
Marco Sanchez <satchito@gmail.com>
2018-08-19 02:12:33 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,6 +6,6 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift(element, ...array);
+  array.unshift(element);
   return array;
 }
