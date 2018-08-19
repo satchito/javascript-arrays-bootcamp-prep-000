bebfe52944349f513f3cbae2a88d9fa68e3b4b81
Marco Sanchez <satchito@gmail.com>
2018-08-19 02:28:56 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -40,8 +40,8 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  array.slice(0, array.lenght - 1);
-  return array;
+  var = newArrayTwo = array.slice(0, array.length - 1);
+  return newArrayTwo;
 }
 
 
