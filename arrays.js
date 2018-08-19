d1f12135943f8797e5c9c059f665fed971eaf170
Marco Sanchez <satchito@gmail.com>
2018-08-19 02:09:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,8 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element) {
-  var nonDestructiveArray = [element, ...array];
-  return array;
+  var newArray = [element, ...array];
+  return newArray;
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
